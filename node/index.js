277f522ea0441b5c8fe162356affc6dfc6929972
shreyasw03 <shreyasw03@gmail.com>
2024-12-28 16:45:03 +0530

    Add files via upload

---
+++ node/index.js
@@ -0,0 +1,17 @@
+// console.log("Hello shreyas, how are you?");
+//jshint esversion:6
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt","file2.txt")
+
+// this is a package of npm which gives name of superheroes
+
+const superheroes = require('superheroes');
+var mysuperheroname = superheroes.random();
+console.log(mysuperheroname);
+
+
+// const supervillains = require('supervillains');
+
+// supervillains.all;
+
+// supervillains.random();
